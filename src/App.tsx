bade625db5d9dec36a99524b8f972338d3dfaff5
MaxenceGitDev <maxence.pruvot@icloud.com>
2025-01-02 11:34:01 +0100

    Quete react 01 fetch depuis une API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-function App() {
+import { useState } from "react";
+import EmployeeCard from "./components/EmployeeCard";
+
+const sampleEmployee = {
+  picture: { medium: "https://randomuser.me/api/portraits/med/men/96.jpg" },
+  name: { first: "John", last: "Doe" },
+  email: "johndoe@mailtest.com",
+};
+
+export default function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+    //send the request
+    fetch("https://www.randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setEmployee(data.results[0]);
+      })
+      .catch((error) => {
+        console.error("Error fetching employee:", error);
+      });
+  };
   return (
     <>
-      <h1>React + Biome = Template</h1>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
     </>
   );
 }
-
-export default App;
